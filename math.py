746cd86e97413d9eb23ecabc39b545bb8af59517
vishakhavishe <vishevishakha97@gmail.com>
2021-03-22 16:40:27 +0530

    function

--- math.py
+++ math.py
@@ -1,18 +1,13 @@
-# hiij
+
+#add 
 def add(x,y):
-   return X+Y
-	# hii
-def subtract(x,y):
-    return X-Y
-def multiply(x,y):
-    return x*y
-def divide(x,y):
-<<<<<<< HEAD
-return X/Y
-=======
-return X-Y
->>>>>>> 003
     pass
-	#square
-	def square(x):
-	pass
+	#sub
+def subtract(x,y):
+    pass
+	#mul
+def multiply(x,y):
+    pass
+	#div
+def divide(x,y):
+pass
