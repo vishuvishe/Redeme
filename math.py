12e1c344d62e1af15b86d864c3c46c5cbd131aa7
vishuvishe <81079243+vishuvishe@users.noreply.github.com>
2021-03-22 15:51:05 +0530

    initial commit

---
+++ math.py
@@ -0,0 +1,18 @@
+# hiij
+def add(x,y):
+   return X+Y
+	# hii
+def subtract(x,y):
+    return X-Y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+<<<<<<< HEAD
+return X/Y
+=======
+return X-Y
+>>>>>>> 003
+    pass
+	#square
+	def square(x):
+	pass
